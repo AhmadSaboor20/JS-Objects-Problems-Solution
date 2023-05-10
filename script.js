813a24021eedcdcc68c3afde50b9f794e5988311
Ahmad Saboor <ahmadsaboor020@gmail.com>
2023-05-10 13:08:00 +0500

    Updated

--- script.js
+++ script.js
@@ -184,137 +184,137 @@ console.log(x)
 
 
 
-// // 8. Write a JavaScript program to create a Clock.
+    // // 8. Write a JavaScript program to create a Clock.
 
-// // Note: The output will come every second.
+    // // Note: The output will come every second.
 
-// // Expected Console Output :
+    // // Expected Console Output :
 
-// // "14:37:42"
+    // // "14:37:42"
 
-// // "14:37:43"
+    // // "14:37:43"
 
-// // "14:37:44"
+    // // "14:37:44"
 
-// // "14:37:45"
+    // // "14:37:45"
 
-// // "14:37:46"
+    // // "14:37:46"
 
-// // "14:37:47"
+    // // "14:37:47"
 
 
 
 
 
-// // 9. Write a JavaScript program to calculate the area and perimeter of a circle.
+    // // 9. Write a JavaScript program to calculate the area and perimeter of a circle.
 
-// // Note : Create two methods to calculate the area and perimeter. The radius of the circle will be supplied by the user.
+    // // Note : Create two methods to calculate the area and perimeter. The radius of the circle will be supplied by the user.
 
 
 
-// // 10. Write a JavaScript program to sort an array of JavaScript objects.
+    // // 10. Write a JavaScript program to sort an array of JavaScript objects.
 
-// // Sample Object :
+    // // Sample Object :
 
 
 
-// // var library = [
+    // // var library = [
 
-// //    {
+    // //    {
 
-// //        title:  'The Road Ahead',
+    // //        title:  'The Road Ahead',
 
-// //        author: 'Bill Gates',
+    // //        author: 'Bill Gates',
 
-// //        libraryID: 1254
+    // //        libraryID: 1254
 
-// //    },
+    // //    },
 
-// //    {
+    // //    {
 
-// //        title: 'Walter Isaacson',
+    // //        title: 'Walter Isaacson',
 
-// //        author: 'Steve Jobs',
+    // //        author: 'Steve Jobs',
 
-// //        libraryID: 4264
+    // //        libraryID: 4264
 
-// //    },
+    // //    },
 
-// //    {
+    // //    {
 
-// //        title: 'Mockingjay: The Final Book of The Hunger Games',
+    // //        title: 'Mockingjay: The Final Book of The Hunger Games',
 
-// //        author: 'Suzanne Collins',
+    // //        author: 'Suzanne Collins',
 
-// //        libraryID: 3245
+    // //        libraryID: 3245
 
-// //    }];
+    // //    }];
 
-// // Expected Output:
+    // // Expected Output:
 
 
 
-// // [[object Object] {
+    // // [[object Object] {
 
-// //   author: "Walter Isaacson",
+    // //   author: "Walter Isaacson",
 
-// //   libraryID: 4264,
+    // //   libraryID: 4264,
 
-// //   title: "Steve Jobs"
+    // //   title: "Steve Jobs"
 
-// // }, [object Object] {
+    // // }, [object Object] {
 
-// //   author: "Suzanne Collins",
+    // //   author: "Suzanne Collins",
 
-// //   libraryID: 3245,
+    // //   libraryID: 3245,
 
-// //   title: "Mockingjay: The Final Book of The Hunger Games"
+    // //   title: "Mockingjay: The Final Book of The Hunger Games"
 
-// // }, [object Object] {
+    // // }, [object Object] {
 
-// //   author: "The Road Ahead",
+    // //   author: "The Road Ahead",
 
-// //   libraryID: 1254,
+    // //   libraryID: 1254,
 
-// //   title: "Bill Gates"
+    // //   title: "Bill Gates"
 
-// // }]
+    // // }]
 
 
 
-// //  11. Write a JavaScript function to print all the methods in an JavaScript object.
+    // //  11. Write a JavaScript function to print all the methods in an JavaScript object.
 
-// // Test Data :
+    // // Test Data :
 
-// // console.log(all_properties(Array));
+    // // console.log(all_properties(Array));
 
-// // ["length", "name", "arguments", "caller", "prototype", "isArray", "observe", "unobserve"]
+    // // ["length", "name", "arguments", "caller", "prototype", "isArray", "observe", "unobserve"]
 
 
 
-// // 12. Write a JavaScript function to parse an URL.
+    // // 12. Write a JavaScript function to parse an URL.
 
 
-// //  13. Write a JavaScript function to retrieve all the names of object's own and inherited properties.
+    // //  13. Write a JavaScript function to retrieve all the names of object's own and inherited properties.
 
-// //  14. Write a JavaScript function to retrieve all the values of an object's properties.
+    // //  14. Write a JavaScript function to retrieve all the values of an object's properties.
 
-//    15. Write a JavaScript function to convert an object into a list of `[key, value]` pairs.
+    //    15. Write a JavaScript function to convert an object into a list of `[key, value]` pairs.
 
         
 
-// //  16. Write a JavaScript function to get a copy of the object where the keys have become the values and the values the keys.
+    // //  16. Write a JavaScript function to get a copy of the object where the keys have become the values and the values the keys.
 
-//  17. Write a JavaScript function to check whether an object contains given property.
-let ahmad = {
-    name: "ali",
-    age: 32,
-    gender: "male"
-}
-function newFunction() {
-    for (val in x) {
-        if (x[val] < x[val + 1]) {
+    //  17. Write a JavaScript function to check whether an object contains given property.
+    let ahmad = {
+        name: "ali",
+        age: 32,
+        gender: "male"
+    }
+    function newFunction() {
+        for (val in x) {
+            if (x[val] < x[val + 1]) {
+            }
         }
     }
-}
 
